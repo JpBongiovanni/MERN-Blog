e13bf83ad80d48ca61ce11a0fc041ad0faad07c7
Jason Bongiovanni <jpbongiovanni@gmail.com>
2021-11-05 16:50:01 -0400

    reducer

---
+++ front_end/src/reducers/posts.js
@@ -0,0 +1,12 @@
+const reducer = (state, action) => {
+    if(action.type === 'CREATE'){
+        switech (action.type){
+            case 'FETCH_ALL':
+                return state;
+            case 'FETCH_ALL':
+                return state;
+            case 'FETCH_ALL':
+                return state;
+        }
+    }
+}
